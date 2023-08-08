17e7b0a2b25f1a43a5006f9cdebe432dc492c605
denis <suddenig@gmail.com>
2023-08-08 18:28:13 +0300

    rewrite tests

--- tests/UI/tests_ui.py
+++ tests/UI/tests_ui.py
@@ -4,27 +4,31 @@ from tests.UI.page_objects.main_page import MainPage
 from tests.UI.page_objects.register_page import RegisterPage
 
 
-@allure.title('first')
+@allure.title('Check the number of categories')
 def test_check_main_page_elements(browser, base_url):
     """
-    Check number of Categories
+    Check the number of Categories
     """
     browser.get(base_url)
-    with allure.step('Check number of Categories'):
-        MainPage(browser).check_len_elements(MainPage.CATEGORIES, 7)
+    main_page = MainPage(browser)
+    with allure.step('Check the number of Categories'):
+        main_page.check_len_elements(MainPage.CATEGORIES, 7)
 
-@allure.title('second')
+
+@allure.title('Register new user')
 def test_register_new_user(browser, base_url):
     """
     Register new user
     """
     browser.get(base_url)
-    with allure.step('Click register button'):
-        MainPage(browser).find_register_btn().click()
-    with allure.step('Fill required fields'):
-        RegisterPage(browser).create_random_user()
-    with allure.step('Click register button'):
-        RegisterPage(browser).find_register_btn().click()
-    with allure.step('Check success registration text'):
-        RegisterPage(browser).check_success_registration()
+    main_page = MainPage(browser)
+    register_page = RegisterPage(browser)
 
+    with allure.step('Click register button'):
+        main_page.find_register_btn().click()
+    with allure.step('Fill required fields'):
+        register_page.create_random_user()
+    with allure.step('Click register button'):
+        register_page.find_register_btn().click()
+    with allure.step('Check success registration text'):
+        register_page.check_success_registration()
